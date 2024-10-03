a52e75a01451da9ce9e348f1d564ed2e06a6234b
ghost173 <jpirakas007@gmail.com>
2024-10-03 22:55:44 +0530

    dummy test case for workflow check

---
+++ src/test/java/com/rest/Test2.java
@@ -0,0 +1,11 @@
+package com.rest;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+
+    @Test
+    public void testa1() {
+        System.out.println("hello method test2");
+    }
+}
